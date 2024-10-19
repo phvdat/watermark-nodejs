09990f9fb5af9fd30050e80d0f86e3e6ec8f4753
phamvdat <dat.pham@lfglobaltech.com>
2024-10-19 15:57:37 +0700

    fix: AxiosError: maxContentLength size of -1 exceeded

--- index.js
+++ index.js
@@ -19,140 +19,146 @@ app.use(cors());
 const upload = multer({ dest: 'uploads/' });
 
 app.post('/process', upload.single('excelFile'), async (req, res) => {
-	res.json({ processing: true });
-	const {
-		logoUrl,
-		logoWidth,
-		logoHeight,
-		imageWidth,
-		imageHeight,
-		quality,
-		idTelegram,
-		shopName,
-	} = req.body;
-	const excelFile = req.file;
+  res.json({ processing: true });
+  const {
+    logoUrl,
+    logoWidth,
+    logoHeight,
+    imageWidth,
+    imageHeight,
+    quality,
+    idTelegram,
+    shopName,
+  } = req.body;
+  const excelFile = req.file;
 
-	// Process the Excel file
-	const workbook = xlsx.readFile(excelFile.path, { type: 'array' });
-	const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-	const rowsRaw = xlsx.utils.sheet_to_json(worksheet);
-	const rows = rowsRaw.filter(row => {
-		return row.Name && row.Images;
-	});
-	const imagesFolderPath = `media/images-${Date.now()}`;
-	fs.mkdirSync(imagesFolderPath, { recursive: true });
-	const zipFileName = `images-${Date.now()}.zip`;
-	const zipFilePath = `./media/${zipFileName}`;
-	try {
-		for (const row of rows) {
-			const regex = /[^a-zA-Z0-9\s]/g;
-			const nameOrigin = row[nameColumn];
-			const name = nameOrigin.replace(regex, '');
-			const imageUrls = row[imagesColumn].split(',');
-			const folderPath = `${imagesFolderPath}/${name}`;
-			fs.mkdirSync(folderPath, { recursive: true });
+  // Process the Excel file
+  const workbook = xlsx.readFile(excelFile.path, { type: 'array' });
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  const rowsRaw = xlsx.utils.sheet_to_json(worksheet);
+  const rows = rowsRaw.filter((row) => {
+    return row.Name && row.Images;
+  });
+  const imagesFolderPath = `media/images-${Date.now()}`;
+  fs.mkdirSync(imagesFolderPath, { recursive: true });
+  const zipFileName = `images-${Date.now()}.zip`;
+  const zipFilePath = `./media/${zipFileName}`;
+  try {
+    console.log(rows);
+    for (const row of rows) {
+      const regex = /[^a-zA-Z0-9\s]/g;
+      const nameOrigin = row[nameColumn];
+      const name = nameOrigin.replace(regex, '');
+      const imageUrls = row[imagesColumn].split(',');
+      const folderPath = `${imagesFolderPath}/${name}`;
+      fs.mkdirSync(folderPath, { recursive: true });
 
-			for (let i = 0; i < imageUrls.length; i++) {
-				const imageUrl = imageUrls[i];
-				const imageName = `${name.replaceAll(' ', '-')}-${i + 1}.jpg`;
-				const imagePath = `${folderPath}/${imageName}`;
+      const logoResponse = await axios.get(logoUrl, {
+        responseType: 'arraybuffer',
+      });
 
-				try {
-					const response = await axios.get(imageUrl, {
-						responseType: 'arraybuffer',
-					});
-					const logoResponse = await axios.get(logoUrl, {
-						responseType: 'arraybuffer',
-					});
-					const resizedLogo = await sharp(logoResponse.data)
-						.resize(Number(logoWidth), Number(logoHeight))
-						.toBuffer();
-					const buffer = await sharp(response.data)
-						.resize(Number(imageWidth), Number(imageHeight))
-						.composite([
-							{
-								input: resizedLogo,
-								gravity: 'southeast',
-							},
-						])
-						.jpeg({ quality: Number(quality) })
-						.toBuffer();
+      const resizedLogo = await sharp(logoResponse.data)
+        .resize(Number(logoWidth), Number(logoHeight))
+        .toBuffer();
 
-					// Save the processed image
-					fs.writeFileSync(imagePath, buffer);
+      for (let i = 0; i < imageUrls.length; i++) {
+        const imageUrl = imageUrls[i];
+        const imageName = `${name.replaceAll(' ', '-')}-${i + 1}.jpg`;
+        const imagePath = `${folderPath}/${imageName}`;
 
-					// Update EXIF metadata of the image
-					addMetadata(name, shopName, imagePath);
-				} catch (error) {
-					console.error(error);
-					continue;
-				}
-			}
-		}
+        try {
+          const response = await axios.get(imageUrl, {
+            responseType: 'arraybuffer',
+            headers: {
+              'accept-encoding': 'gzip, deflate',
+            },
+          });
+          const buffer = await sharp(response.data)
+            .resize(Number(imageWidth), Number(imageHeight))
+            .composite([
+              {
+                input: resizedLogo,
+                gravity: 'southeast',
+              },
+            ])
+            .jpeg({ quality: Number(quality) })
+            .toBuffer();
 
-		const output = fs.createWriteStream(zipFilePath);
-		const archive = archiver('zip', { zlib: { level: 9 } });
-		output.on('close', () => {
-			const downloadLink = `${process.env.REACT_APP_API_ENDPOINT}/${zipFileName}`;
-			const message = `Click the link below to download the processed images:\n${downloadLink} \nLink will be expired in 5 hours`;
-			bot
-				.sendMessage(idTelegram, message)
-				.then(() => {
-					// fs.unlinkSync(excelFile.path);
-					deleteFolderRecursive(imagesFolderPath);
-					const deletionTime = 5 * 60 * 60 * 1000; // 10 hours
-					setTimeout(() => {
-						fs.unlinkSync(zipFilePath);
-					}, deletionTime);
-				})
-				.catch((error) => {
-					console.error('Error sending download link to Telegram:', error);
-					fs.unlinkSync(excelFile.path);
-					deleteFolderRecursive(imagesFolderPath);
-					fs.unlinkSync(zipFilePath);
-				});
-		});
+          // Save the processed image
+          fs.writeFileSync(imagePath, buffer);
 
-		archive.on('error', (err) => {
-			throw err;
-		});
+          // Update EXIF metadata of the image
+          addMetadata(name, shopName, imagePath);
+        } catch (error) {
+          console.error(error.response);
+          continue;
+        }
+      }
+    }
 
-		archive.pipe(output);
-		archive.directory(imagesFolderPath, false);
-		archive.finalize();
-	} catch (error) {
-		res.status(500);
-		fs.unlinkSync(excelFile.path);
-		deleteFolderRecursive(imagesFolderPath);
-	}
+    const output = fs.createWriteStream(zipFilePath);
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    output.on('close', () => {
+      const downloadLink = `${process.env.REACT_APP_API_ENDPOINT}/${zipFileName}`;
+      const message = `Click the link below to download the processed images:\n${downloadLink} \nLink will be expired in 5 hours`;
+      bot
+        .sendMessage(idTelegram, message)
+        .then(() => {
+          // fs.unlinkSync(excelFile.path);
+          deleteFolderRecursive(imagesFolderPath);
+          const deletionTime = 5 * 60 * 60 * 1000; // 10 hours
+          setTimeout(() => {
+            fs.unlinkSync(zipFilePath);
+          }, deletionTime);
+        })
+        .catch((error) => {
+          console.error('Error sending download link to Telegram:', error);
+          fs.unlinkSync(excelFile.path);
+          deleteFolderRecursive(imagesFolderPath);
+          fs.unlinkSync(zipFilePath);
+        });
+    });
+
+    archive.on('error', (err) => {
+      throw err;
+    });
+
+    archive.pipe(output);
+    archive.directory(imagesFolderPath, false);
+    archive.finalize();
+  } catch (error) {
+    res.status(500);
+    fs.unlinkSync(excelFile.path);
+    deleteFolderRecursive(imagesFolderPath);
+  }
 });
 
 function deleteFolderRecursive(folderPath) {
-	if (fs.existsSync(folderPath)) {
-		fs.readdirSync(folderPath).forEach((file) => {
-			const curPath = `${folderPath}/${file}`;
-			if (fs.lstatSync(curPath).isDirectory()) {
-				deleteFolderRecursive(curPath);
-			} else {
-				fs.unlinkSync(curPath);
-			}
-		});
+  if (fs.existsSync(folderPath)) {
+    fs.readdirSync(folderPath).forEach((file) => {
+      const curPath = `${folderPath}/${file}`;
+      if (fs.lstatSync(curPath).isDirectory()) {
+        deleteFolderRecursive(curPath);
+      } else {
+        fs.unlinkSync(curPath);
+      }
+    });
 
-		// Check if the directory is empty after deleting all files and subdirectories
-		const files = fs.readdirSync(folderPath);
-		if (files.length === 0) {
-			fs.rmdirSync(folderPath);
-		} else {
-			console.log(`Directory ${folderPath} is not empty.`);
-		}
-	}
+    // Check if the directory is empty after deleting all files and subdirectories
+    const files = fs.readdirSync(folderPath);
+    if (files.length === 0) {
+      fs.rmdirSync(folderPath);
+    } else {
+      console.log(`Directory ${folderPath} is not empty.`);
+    }
+  }
 }
 
 app.get('/:zipFileName', (req, res) => {
-	const zipFileName = req.params.zipFileName;
-	res.download(`media/${zipFileName}`);
+  const zipFileName = req.params.zipFileName;
+  res.download(`media/${zipFileName}`);
 });
 
 app.listen(process.env.PORT, () => {
-	console.log('Server is running on port', process.env.PORT);
+  console.log('Server is running on port', process.env.PORT);
 });
